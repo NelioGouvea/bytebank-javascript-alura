a8a52b5b91f478fcd1b02cf228b3cf94c62aec64
Nélio Gouvêa <nelioalvesogouvea@gmail.com>
2023-03-07 19:33:58 -0300

    Finaliza capítulo 4

--- script/scripts.js
+++ script/scripts.js
@@ -13,15 +13,6 @@ const graficoParaDolar  = new Chart(graficoDolar, {
         }]
     },
 });
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorario();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao("dolar", valor);
-}
 
 function geraHorario() {
     let date = new Date();
@@ -35,4 +26,14 @@ function adicionarDados(grafico, legenda, dados) {
     })
     grafico.update();
 }
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorario();
+    let valor = event.data.ask;
+    imprimeCotacao("dolar", valor);
+    adicionarDados(graficoParaDolar, tempo, valor);
+})
 
